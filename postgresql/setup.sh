fd3113e75019490775e70da22be791cc395b58f9
sapetnioc <sapetnioc@users.noreply.github.com>
2023-10-18 16:48:55 +0200

    Uncomment Postgresql setup script

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -5,41 +5,41 @@ db_name=db
 
 target=`realpath "$1"`
 
-# mkdir "$target"
-# git  -C "$target" clone git://git.andreasbaumann.cc/pgfuse.git
-# cd "$target"
-# cd ..
-# if [ ! -e Mambaforge-Linux-x86_64.sh ]; then
-#     wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-# fi
-# sh Mambaforge-Linux-x86_64.sh -b -p "$target/conda"
-# "$target/conda/bin/mamba" update -y mamba
-# "$target/conda/bin/mamba" install -y postgresql 'libfuse<3' libpq
+mkdir "$target"
+git  -C "$target" clone git://git.andreasbaumann.cc/pgfuse.git
+cd "$target"
+cd ..
+if [ ! -e Mambaforge-Linux-x86_64.sh ]; then
+    wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+fi
+sh Mambaforge-Linux-x86_64.sh -b -p "$target/conda"
+"$target/conda/bin/mamba" update -y mamba
+"$target/conda/bin/mamba" install -y postgresql 'libfuse<3' libpq
 
 activate_script=`"$target/conda/bin/conda" shell.posix activate base`
 eval "$activate_script"
 
 set -x
 
-# cat > "$target/conda/lib/pkgconfig/fuse.pc" << END
-# prefix=${target}/conda
-# exec_prefix=\${prefix}
-# libdir=\${prefix}/lib
-# includedir=\${prefix}/include
+cat > "$target/conda/lib/pkgconfig/fuse.pc" << END
+prefix=${target}/conda
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+includedir=\${prefix}/include
 
-# Name: fuse
-# Description: Filesystem in Userspace
-# Version: 2.9.9
-# Libs: -L\${libdir} -lfuse -pthread
-# Libs.private: -ldl  
-# Cflags: -I\${includedir}/fuse -D_FILE_OFFSET_BITS=64
-# END
+Name: fuse
+Description: Filesystem in Userspace
+Version: 2.9.9
+Libs: -L\${libdir} -lfuse -pthread
+Libs.private: -ldl  
+Cflags: -I\${includedir}/fuse -D_FILE_OFFSET_BITS=64
+END
 
-# echo "CFLAGS += -I$target/conda/include" >> "$target/pgfuse/inc.mak"
+echo "CFLAGS += -I$target/conda/include" >> "$target/pgfuse/inc.mak"
 
-# cd "$target/pgfuse"
-# make
-# make install prefix="$target/conda"
+cd "$target/pgfuse"
+make
+make install prefix="$target/conda"
 
 mkdir "$target/database"
 initdb --set port=$db_port "$target/database"
